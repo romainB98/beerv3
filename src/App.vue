777512c10258407e5a41a942789a1880722c65fe
romainB98 <56411743+romainB98@users.noreply.github.com>
2019-11-03 23:50:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/beerLogo.png">
+    <beer msg="Glossary of Beer"/>
+    <Listbeer msg="Choose your Own Beer"/>
+    <listbeer info/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import beer from './components/Beer.vue';
+import Listbeer from './components/ListBeer.vue';
+
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    beer,
+    Listbeer,
+    
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListBeer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="Listbeer">
+    <h4>{{ info }}</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+
+export default Vue.extend({
+  name: 'Listbeer',
+  props: {
+    msg: String,
+  },
+});
+
+ new Vue({  
+   el:'listBeer',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://sandbox-api.brewerydb.com/v2/beers/?key=3e3c1e4b6f4eaa862efa14b8a1009582')
+      .then(response => (this.info = response.data.beers))
+      
+  }
+})
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
